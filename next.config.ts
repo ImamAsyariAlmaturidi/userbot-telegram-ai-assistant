a9361f4ecc29824aa0f46c0d8f40f47537b42636
Imam Asyari <imamasyari700@gmail.com>
2025-11-15 08:25:36 +0700

    fix: conflict

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   async rewrites() {
     const target = process.env.API_BASE_URL || "http://localhost:3000";
     return [
